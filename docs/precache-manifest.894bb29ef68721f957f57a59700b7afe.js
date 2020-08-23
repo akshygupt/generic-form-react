d9cfd7fda69e3b03007ea5ce60c6a3bb32124cb7
akshay gupta <akshgupt@redhat.com>
2020-08-23 18:16:17 +0530

    Added docs config

---
+++ docs/precache-manifest.894bb29ef68721f957f57a59700b7afe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740d63c0cd3a1a9ee012a9b4d71d6881",
+    "url": "/generic-form-react/index.html"
+  },
+  {
+    "revision": "247f5a7340333cc977c9",
+    "url": "/generic-form-react/static/css/main.ab347543.chunk.css"
+  },
+  {
+    "revision": "71e029984ae3867e7e81",
+    "url": "/generic-form-react/static/js/2.881e7033.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/generic-form-react/static/js/2.881e7033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247f5a7340333cc977c9",
+    "url": "/generic-form-react/static/js/main.760e0243.chunk.js"
+  },
+  {
+    "revision": "746c86844054d6c06639",
+    "url": "/generic-form-react/static/js/runtime-main.010a7907.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.846593bf9d56b19eed97d1c0f36978bf.js"
+  "/generic-form-react/precache-manifest.894bb29ef68721f957f57a59700b7afe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/generic-form-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
