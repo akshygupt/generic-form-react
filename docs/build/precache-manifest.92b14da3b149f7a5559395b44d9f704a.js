daa87ba115b1f95c7ed514e1a37a92c5b7381ee9
akshay gupta <akshgupt@redhat.com>
2020-08-23 18:06:46 +0530

    Added homepage

---
+++ docs/build/precache-manifest.92b14da3b149f7a5559395b44d9f704a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3509d63795590e2e039c539e8b5449",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b892ce8c65d753190e1c",
+    "url": "./static/css/main.ab347543.chunk.css"
+  },
+  {
+    "revision": "71e029984ae3867e7e81",
+    "url": "./static/js/2.881e7033.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.881e7033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b892ce8c65d753190e1c",
+    "url": "./static/js/main.442b4cd2.chunk.js"
+  },
+  {
+    "revision": "267ba582ea825ccdc440",
+    "url": "./static/js/runtime-main.2a3fa66c.js"
+  }
+]);
