fde2f1f83aedb4ad7088d86f4fa44a78b01ab257
akshay gupta <akshgupt@redhat.com>
2020-08-23 18:12:11 +0530

    Updates

---
+++ precache-manifest.846593bf9d56b19eed97d1c0f36978bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea67ed144c520f70a334ec2e01f64047",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6b19806049fe3559af0",
+    "url": "/static/css/main.ab347543.chunk.css"
+  },
+  {
+    "revision": "71e029984ae3867e7e81",
+    "url": "/static/js/2.881e7033.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.881e7033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b19806049fe3559af0",
+    "url": "/static/js/main.b2248d27.chunk.js"
+  },
+  {
+    "revision": "b3664d893ac782e2aae4",
+    "url": "/static/js/runtime-main.e19dd3f6.js"
+  }
+]);
